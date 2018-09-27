4c278726e6ac3d8a78ab7a2561695352be49efef
Gordon Freeman <wwa1636@web.de>
2018-09-27 03:19:48 +0200

    challenge 5

--- js/script.js
+++ js/script.js
@@ -5,28 +5,58 @@ console.log("App is alive");
  * #6 #Switcher function for the #channels name in the right app bar
  * @param channelName Text which is set
  */
+
+var currentChannel = sevenContinents;
+var currentLocation = [48.265126, 11.669230, "ausbau.weltbild.abweichen"];
+console.log(currentLocation);
+
+
 function switchChannel(channelName) {
     //Log the channel switch
     console.log("Tuning in to channel", channelName);
 
     //Write the new channel to the right app bar
-    document.getElementById('channel-name').innerHTML = channelName;
+    document.getElementById('channel-name').innerHTML = channelName.name;
 
     //#6 change the #channel #location
-    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>';
+    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/minus.plus.yummy" target="_blank"><strong>minus.plus.yummy</strong></a>';
 
-    /* #6 #liking channels on #click */
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+// get the correct star in the app bar
+var checkIfStarred =  (channelName.starred ? true : false);
+    console.log(checkIfStarred);
+if (checkIfStarred === true){
+  $('#channel-star').addClass("fas fa-star");
+} else {
+  $('#channel-star').removeClass("fas fa-star");
+  $('#channel-star').addClass("far fa-star");
+ }
+
 
     /* #6 #highlight the selected #channel.
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
-    $('#channels li:contains(' + channelName + ')').addClass('selected');
+    $('#channels li:contains(' + channelName.name + ')').addClass('selected');
+
+currentChannel = channelName;
+console.log("current channel", currentChannel);
+
+
+
+
 }
 
 /* #6 #liking a channel on #click */
 function star() {
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png');
+  /*  toggles between two classes solid/regular star */
+  $('#channel-star').toggleClass("fas fa-star far fa-star");
+  currentChannel.starred = !currentChannel.starred;
+  console.log("is favorite now", currentChannel.starred);
+  console.log("currentChannelName", currentChannel.name);
+
+
+  $('#channels li:contains(' + currentChannel.name + ')').toggleClass("fas fa-star far fa-star");
+
+
 }
 
 /**
